efd30fbbabf3537be2fab959241094e6fd9ec97c
Tomohisa Yamashita <>
2020-04-06 00:27:09 +0900

    ダイアログ出して、次のページのPushまでできた。

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,63 @@
 import 'package:flutter/material.dart';
 import 'view/top.dart';
 import 'view/trend.dart';
-import 'view/tag.dart';
+import 'view/test.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class DynamicTabContent {
+  String tabTitle;
+  Widget view;
+
+  DynamicTabContent.name(this.tabTitle, this.view);
+}
+
+class MyApp extends StatefulWidget {
+  static final navKey = new GlobalKey<NavigatorState>();
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  List<DynamicTabContent> myList = new List();
+
+  final List<Tab> tabs = <Tab>[
+    Tab(text: 'HOME'),
+    Tab(text: 'トレンド'),
+    Tab(text: 'タグ一覧'),
+  ];
+
+  TabController _tabController;
+  final searchTextController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    myList.add(new DynamicTabContent.name("HOME2", new TopTab('top')));
+    myList.add(new DynamicTabContent.name('トレンド2', new TrendTab('test')));
+    myList.add(new DynamicTabContent.name('タグ一覧2', new TopTab('top')));
+    _tabController = TabController(vsync: this, length: tabs.length);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: MyApp.navKey,
       home: DefaultTabController(
         // タブの数
-        length: 3,
+        length: myList.length,
         child: Scaffold(
           appBar: AppBar(
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'sss',
+                onPressed: buttonPressed,
+//                onPressed: () async {
+//                  Duration date = await showTimerDialog(context: context);
+//                },
+              ),
+            ],
             bottom: TabBar(
               // タブのオプション
               isScrollable: true,
@@ -24,30 +68,60 @@ class MyApp extends StatelessWidget {
               indicatorColor: Colors.white,
               indicatorWeight: 2.0,
               // タブに表示する内容
-              tabs: [
-                Tab(
-                  child: Text('Top'),
-                ),
-                Tab(
-                  child: Text('トレンド'),
-                ),
-                Tab(
-                  child: Text('新着'),
-                ),
-              ],
+              controller: _tabController,
+
+              tabs: myList.map((DynamicTabContent choice) {
+                return Tab(
+                  text: choice.tabTitle,
+                );
+              }).toList(),
             ),
             title: Text('見るだけ！ Qiita Client '),
           ),
           body: TabBarView(
             // 各タブの内容
-            children: [
-              new TopTab('top'),
-              new TrendTab('test'),
-              new TagTab(),
-            ],
+            controller: _tabController,
+            children:
+                myList.map((DynamicTabContent choice) => choice.view).toList(),
           ),
         ),
       ),
+    );
+  }
+
+  void buttonPressed() {
+//    final searchTextController = TextEditingController();
+    final context = MyApp.navKey.currentState.overlay.context;
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('サンプルダイアログ'),
+          content: TextField(
+            controller: searchTextController,
+            decoration: InputDecoration(
+              hintText: "sec",
+            ),
+            autofocus: true,
+            keyboardType: TextInputType.number,
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("CANCEL"),
+              onPressed: () => Navigator.pop(context),
+            ),
+            FlatButton(
+              child: Text("OK"),
+//              onPressed: () => Navigator.pop(context),
+              onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+//                                      builder: (context) => WebViewContainer("https://yahoo.co.jp")
+                      builder: (context) => TestTab("aaaa"))),
+            ),
+          ],
+        );
+      },
     );
   }
 }
